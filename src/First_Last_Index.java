779d52ce7d87eb387872878476fb421c6ba689c9
singh-deepak7 <singh-deepak@outlook.com>
2022-01-28 11:02:41 -0600

    First_Last_Index added code for all indexes

--- src/First_Last_Index.java
+++ src/First_Last_Index.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class First_Last_Index {
     public static void main(String[] args) {
         int arr[] = new int[]{2, 3, 4, 2, 3, 6};
@@ -12,7 +14,8 @@ public class First_Last_Index {
         }
         System.out.println(firstIndex(arr, 3, 0));
         System.out.println(LastIndex(arr, 3, arr.length - 1));
-
+        int[] r = AllIndex(arr, 3, 0, 0);
+        Arrays.stream(r).forEach(x -> System.out.print(x + " "));
     }
 
     // For
@@ -34,6 +37,21 @@ public class First_Last_Index {
             return -1;
         }
         return arr[index] == digit ? index : LastIndex(arr, digit, index - 1);
+    }
+
+    private static int[] AllIndex(int[] arr, int digit, int index, int fsf) {
+        if (arr.length == index) {
+            return new int[fsf];
+        }
+        if( arr[index] == digit){
+            fsf += 1;
+        }
+        int[] faith = AllIndex(arr, digit, index + 1, fsf);
+        if( arr[index] == digit){
+            faith[fsf -1] = index;
+        }
+        return faith;
+
     }
 
 }
