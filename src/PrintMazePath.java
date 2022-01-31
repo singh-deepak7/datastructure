55f0c517da750604614beed33f23d6630469f91a
singh-deepak7 <singh-deepak@outlook.com>
2022-01-31 11:34:40 -0600

    PrintMazePath

---
+++ src/PrintMazePath.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrintMazePath {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int r = scn.nextInt();
+        int c = scn.nextInt();
+        printMazePaths(0, 0, r - 1, c - 1, "");
+
+    }
+
+    // sr - source row
+    // sc - source column
+    // dr - destination row
+    // dc - destination column
+    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+        if (sr > dr || sc > dc) return;
+        if (sr == dr && sc == dc) {
+            System.out.println(psf);
+            return;
+        }
+
+        printMazePaths(sr, sc + 1, dr, dc, psf + "h");
+        printMazePaths(sr + 1, sc, dr, dc, psf + "v");
+
+    }
+}
