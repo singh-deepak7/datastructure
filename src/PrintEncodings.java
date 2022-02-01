92271be069142132730abd45c43032f77ff30836
singh-deepak7 <singh-deepak@outlook.com>
2022-02-01 13:41:20 -0600

    PrintEncodings

---
+++ src/PrintEncodings.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PrintEncodings {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        printEncodings(scn.nextLine(), "");
+    }
+
+    public static void printEncodings(String str, String ans) {
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+        int n = str.charAt(0) - '0';
+        if(n ==0 ) return; // leading zero
+        printEncodings(str.substring(1), ans + (char)('a' + n -1));
+        if(str.length() > 1){
+            n = Integer.parseInt(str.substring(0, 2));
+            if( n < 26){
+                printEncodings(str.substring(2), ans + (char)('a' + n -1));
+            }
+        }
+
+    }
+}
