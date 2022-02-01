74912f87fffa1b43160c855ea0c8f8301196f6c6
singh-deepak7 <singh-deepak@outlook.com>
2022-02-01 13:41:42 -0600

    PrintEncodings

--- src/PrintEncodings.java
+++ src/PrintEncodings.java
@@ -7,17 +7,17 @@ public class PrintEncodings {
     }
 
     public static void printEncodings(String str, String ans) {
-        if(str.length() == 0){
+        if (str.length() == 0) {
             System.out.println(ans);
             return;
         }
         int n = str.charAt(0) - '0';
-        if(n ==0 ) return; // leading zero
-        printEncodings(str.substring(1), ans + (char)('a' + n -1));
-        if(str.length() > 1){
+        if (n == 0) return; // leading zero
+        printEncodings(str.substring(1), ans + (char) ('a' + n - 1));
+        if (str.length() > 1) {
             n = Integer.parseInt(str.substring(0, 2));
-            if( n < 26){
-                printEncodings(str.substring(2), ans + (char)('a' + n -1));
+            if (n < 26) {
+                printEncodings(str.substring(2), ans + (char) ('a' + n - 1));
             }
         }
 
