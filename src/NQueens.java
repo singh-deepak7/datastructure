bcf47588766edb14402d3392c0d42854c1d59999
singh-deepak7 <singh-deepak@outlook.com>
2022-02-03 06:57:33 -0600

    NQueens

--- src/NQueens.java
+++ src/NQueens.java
@@ -10,20 +10,28 @@ public class NQueens {
 
     public static void printNQueens(int[][] chess, String qsf, int row) {
         if (row == chess.length) {
-            System.out.println(qsf);
+            System.out.println(qsf + ".");
             return;
         }
 
         for (int col = 0; col < chess.length; col++) {
-            if (isSafeToPlace() == true) {
+            if (isSafeToPlace(chess, row, col) == true) {
                 chess[row][col] = 1;
-                printNQueens(chess, (qsf + row + "-" + col + ", "), row + 1;
+                printNQueens(chess, (qsf + row + "-" + col + ", "), row + 1);
                 chess[row][col] = 0;
             }
         }
 
     }
 
-    private static boolean isSafeToPlace() {
+    private static boolean isSafeToPlace(int[][] chess, int r, int c) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < chess[0].length; j++) {
+                // diagonal check - major diff is same, minor sum is same
+                if ((i + j == r + c || (i - j == r - c)) && chess[i][j] == 1) return false;
+                if (chess[i][c] == 1) return false;
+            }
+        }
+        return true;
     }
 }
