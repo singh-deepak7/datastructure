cfafeb13f7fd17ea8777bcdf619e01eb5a8ab824
singh-deepak7 <singh-deepak@outlook.com>
2022-02-18 10:50:39 -0600

    CanSeePersonCount

---
+++ src/CanSeePersonCount.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class CanSeePersonCount {
+
+  /*  Your input [10,6,8,5,11,9]
+    Output [3,1,2,1,1,0]*/
+
+    //https://leetcode.com/problems/number-of-visible-people-in-a-queue/
+}
+
+
+class Solution {
+    public int[] canSeePersonsCount(int[] heights) {
+        Stack<Integer> st = new Stack<>();
+        int n = heights.length;
+        int[] ans = new int[n];
+        for(int i = n-1; i >= 0; i --){
+            int pc = 0;
+            while(!st.isEmpty() && st.peek() < heights[i]){
+                st.pop();
+                pc++;
+            }
+            ans[i] = pc + (st.isEmpty() ? 0 : 1);
+            st.push(heights[i]);
+        }
+        return ans;
+
+    }
+}
--- src/Stacks.java
+++ src/Stacks.java
@@ -7,14 +7,51 @@ public class Stacks {
         String matchinIinput = "(a+b)+(c+d)";
 
         int[] arr = new int[]{5, 5, 3, 8, -2, 7};
-       int [] ans = nextMaxRight(arr);
+        int[] ans = nextMaxRight(arr);
         Arrays.stream(ans).forEach(x -> System.out.print(x + " "));
 
+        System.out.println(" ----------");
+        int[] arr_1 = new int[]{1, 3, 5, 4, 1};
+        int[] ans_1 = nsl(arr_1);
+        Arrays.stream(ans_1).forEach(x -> System.out.print(x + " "));
+        System.out.println("--- ngl --");
 
+        int[] arr_2 = new int[]{3,1,2,3,4,2,2,1,0,3};
+        int[] ans_2 = ngl(arr_2);
+        Arrays.stream(ans_2).forEach(x -> System.out.print(x + " "));
+        
+        System.out.println(" ----------");
         String input = "[(a + b) + {(c + d) * (e / f)}]}";
         System.out.println("isMatching " + isMatching(matchinIinput));
         System.out.println("isBalancedBracket " + isBalancedBracket(input));
 
+    }
+
+    private static int[] ngl(int[] arr_2) {
+        int n = arr_2.length;
+        int[] span = new int[n];
+        Stack<Integer> st = new Stack<>();
+
+        for(int i =0 ; i < n; i ++){
+            while (!st.isEmpty() && arr_2[st.peek()] <= arr_2[i]) st.pop();
+            span[i] = i - (st.isEmpty() ? -1 : st.peek());
+            st.push(i);
+        }
+        return span;
+    }
+
+    //-1 1 3 3 -1
+    private static int[] nsl(int[] arr) {
+
+        int n = arr.length;
+        Stack<Integer> st = new Stack<>();
+        int[] ans = new int[n];
+        for (int i = 0; i <= n - 1; i++) {
+            while (!st.isEmpty() && st.peek() >= arr[i]) st.pop();
+            ans[i] = st.isEmpty() ? -1 : i;
+            st.push(arr[i]);
+        }
+        return ans;
     }
 
     private static int[] nextMaxRight(int[] arr) {
@@ -22,7 +59,7 @@ public class Stacks {
         Stack<Integer> st = new Stack<>();
         int[] ans = new int[n];
         for (int i = n - 1; i >= 0; i--) {
-            while( !st.isEmpty() && st.peek() <= arr[i]) st.pop();
+            while (!st.isEmpty() && st.peek() <= arr[i]) st.pop();
             ans[i] = st.isEmpty() ? -1 : st.peek();
             st.push(arr[i]);
         }
