24b0ab972751b6f16ce655795c5e05a9d2aab3ef
singh-deepak7 <singh-deepak@outlook.com>
2022-02-19 08:09:13 -0600

    SimplifyPath

--- src/AsteroidCollision.java
+++ src/AsteroidCollision.java
@@ -12,20 +12,20 @@ https://leetcode.com/problems/asteroid-collision/submissions/
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> st = new Stack<>();
-        for(int i : asteroids){
-            if( i > 0){
+        for (int i : asteroids) {
+            if (i > 0) {
                 st.push(i);
-            } else{
-                while(!st.isEmpty() && st.peek() > 0 && -i >st.peek()) st.pop();
-                if(st.isEmpty() || st.peek() < 0) st.push(i);
-                if(st.peek() == -i) st.pop();
+            } else {
+                while (!st.isEmpty() && st.peek() > 0 && -i > st.peek()) st.pop();
+                if (st.isEmpty() || st.peek() < 0) st.push(i);
+                if (st.peek() == -i) st.pop();
 
             }
         }
         int s = st.size();
         int i = 0;
         int[] ans = new int[s];
-        for(int x: st){
+        for (int x : st) {
             ans[i++] = x;
         }
         return ans;
---
+++ src/SimplifyPath.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+public class SimplifyPath {
+/*
+    Your input "/home/"
+    Output "/home"
+    https://leetcode.com/problems/simplify-path/submissions/
+
+    */
+}
+
+
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> st = new Stack<>();
+        for (String s : path.split("/")) {
+            if (s.length() > 0 && !s.equals(".")) {
+                if (s.equals("..")) {
+                    if (!st.isEmpty()) st.pop();
+                } else st.push(s);
+            }
+        }
+        if (st.isEmpty()) return "/";
+        StringBuilder sb = new StringBuilder();
+        for (String s : st) {
+            sb.append("/" + s);
+        }
+        return sb.toString();
+
+    }
+}
