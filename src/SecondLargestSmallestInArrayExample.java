8710746ca93cd4947d89c6849aa7c0e8bc87c355
singh-deepak7 <singh-deepak@outlook.com>
2022-02-24 16:08:13 -0600

    SecondLargestSmallestInArrayExample

---
+++ src/SecondLargestSmallestInArrayExample.java
@@ -0,0 +1,45 @@
+public class SecondLargestSmallestInArrayExample {
+    public static int getSecondLargest(int[] a){
+        int total = a.length;
+        int temp;
+        for (int i = 0; i < total; i++)
+        {
+            for (int j = i + 1; j < total; j++)
+            {
+                if (a[i] > a[j])
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        return a[total-2];
+    }
+
+    public static int getSecondSmallest(int[] a){
+        int total = a.length;
+        int temp;
+        for (int i = 0; i < total; i++)
+        {
+            for (int j = i + 1; j < total; j++)
+            {
+                if (a[i] > a[j])
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        return a[1];
+    }
+    public static void main(String args[]){
+        int[] a={1,2,5,6,3,2};
+        int[] b={44,66,99,77,33,22,55};
+        System.out.println("Second Largest: "+getSecondLargest(a));
+        System.out.println("Second Largest: "+getSecondLargest(b));
+
+        System.out.println("Second Smallest: "+getSecondSmallest(a));
+        System.out.println("Second Smallest: "+getSecondSmallest(b));
+    }}
