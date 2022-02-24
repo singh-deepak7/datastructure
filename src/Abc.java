a7977e38b3f47af8fa9cb2c39838b99cc5776822
singh-deepak7 <singh-deepak@outlook.com>
2022-02-24 08:28:28 -0600

    updates

--- src/Abc.java
+++ src/Abc.java
@@ -9,5 +9,39 @@ public class Abc {
             num = num / 10;
         }
         System.out.println(reverseAnswer);
+
+
+        ///
+
+        String input = "wwwwaaadexxxxxx";
+        StringBuilder sb = new StringBuilder();
+        sb.append(input.charAt(0));
+        for (int i = 1; i < input.length(); i++) {
+            char prev = input.charAt(i - 1);
+            char curr = input.charAt(i);
+            if (prev != curr) {
+                sb.append(curr);
+            }
+        }
+        System.out.println(sb.toString());
+
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append(input.charAt(0));
+        int counter = 1;
+        for (int i = 1; i < input.length(); i++) {
+            char prev = input.charAt(i - 1);
+            char curr = input.charAt(i);
+            if (prev != curr) {
+                sb1.append(counter != 1 ? counter : "");
+                sb1.append(curr);
+                counter = 1;
+            } else {
+                counter++;
+            }
+        }
+        sb1.append(counter != 1 ? counter : "");
+        System.out.println(sb1.toString());
+
+
     }
 }
--- src/ThreeNumberSum.java
+++ src/ThreeNumberSum.java
@@ -5,26 +5,27 @@ import java.util.List;
 public class ThreeNumberSum {
 
     public static void main(String[] args) {
-        System.out.println(threeNumberSum(new int[] { 12, 3, 1, 2, -6, 5, -8, 6 }, 0));
-        List<Integer[]> triplets = threeNumberSum(new int[] { 12, 3, 1, 2, -6, 5, -8, 6 }, 0);
+        System.out.println(threeNumberSum(new int[]{12, 3, 1, 2, -6, 5, -8, 6}, 0));
+        List<Integer[]> triplets = threeNumberSum(new int[]{12, 3, 1, 2, -6, 5, -8, 6}, 0);
 
-
+        for (Integer[] t : triplets) {
+            for (int n : t) {
+                System.out.print(n + ",");
+            }
+            System.out.println();
+        }
     }
 
     private static List<Integer[]> threeNumberSum(int[] arrays, int targetSum) {
-
         Arrays.sort(arrays);
-
         List<Integer[]> triplets = new ArrayList<Integer[]>();
-
         for (int i = 0; i < arrays.length - 2; i++) {
             int left = i + 1;
             int right = arrays.length - 1;
             while (left < right) {
-
                 int currentSum = arrays[i] + arrays[left] + arrays[right];
                 if (currentSum == targetSum) {
-                    Integer[] newTriplet = { arrays[i], arrays[left], arrays[right] };
+                    Integer[] newTriplet = {arrays[i], arrays[left], arrays[right]};
                     triplets.add(newTriplet);
                     left++;
                     right--;
